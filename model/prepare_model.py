f01c0dcc76e570c04b242eedacf0b78206d460ac
Junxen <Junxen@users.noreply.github.com>
2022-09-07 17:07:06 +0800

    add mae to feature_feature_extracting

---
+++ model/prepare_model.py
@@ -0,0 +1,12 @@
+import torch
+import models_mae
+
+
+def prepare_model(chkpt_dir, arch='mae_vit_large_patch16'):
+    # build model
+    model = getattr(models_mae, arch)()
+    # load model
+    checkpoint = torch.load(chkpt_dir, map_location='cpu')
+    msg = model.load_state_dict(checkpoint['model'], strict=False)
+    print(msg)
+    return model
